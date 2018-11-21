6e5c598dad672be630616c95ddeeb6cf40c96b44
salagic-tgm <salagic@student.tgm.ac.at>
2018-11-21 09:01:37 +0100

    Revert "Merge branch 'salagic'"
    This reverts commit 54e8fe293f6a004a590c56cb285dda87c4b9d6f6, reversing
    changes made to bbe5316a426cc58fbb748b045119bfc23ead497b.

--- MEVA/src/routes/dashboard.jsx
+++ MEVA/src/routes/dashboard.jsx
@@ -47,11 +47,7 @@ const dashboardRoutes = [
   {
     path: "/icons",
     sidebarName: "Loaned Products",
-<<<<<<< HEAD
     navbarName: "Loaned Products",
-=======
-    navbarName: "Icons",
->>>>>>> salagic
     icon: BubbleChart,
     component: Icons
   },
--- MEVA/src/views/Hardware/Hardware.jsx
+++ MEVA/src/views/Hardware/Hardware.jsx
@@ -10,10 +10,7 @@ import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import AccessTime from "@material-ui/core/SvgIcon/SvgIcon";
 import CardFooter from "components/Card/CardFooter.jsx";
-<<<<<<< HEAD
 import Button from '@material-ui/core/Button';
-=======
->>>>>>> salagic
 
 const styles = {
   cardCategoryWhite: {
@@ -48,20 +45,11 @@ const styles = {
 function Hardware(props) {
   const { classes } = props;
   return (
-<<<<<<< HEAD
     <GridContainer classes={{container: classes.gridContainer}}>
         <GridItem xs={12} sm={6} md={4}>
             <Card chart>
                 <CardHeader>
                     <img src={ require('../../assets/img/druckerxy.jpg') } width={"100%"} height={"100%"} mode="fit" />
-=======
-    <GridContainer>
-
-        <GridItem xs={12} sm={6} md={4}>
-            <Card chart>
-                <CardHeader>
-                    <img src={ require('../../assets/img/PIXMA MX925.jpg') } width={"100%"} height={"100%"} mode="fit" />
->>>>>>> salagic
                 </CardHeader>
                 <CardBody>
                     <h4 className={classes.cardTitle}>Produkt XY</h4>
@@ -71,7 +59,6 @@ function Hardware(props) {
                     <p>Kategorie:</p><p> Drucker</p>
                     <a href="https://cdn.billiger.com/dynimg/BeJSIoTIRwE6Ls_wbObPEXt4H9rycHjeBa9LTdXG2DAE0q0O1YuZqiNdvIY_Nfu9H6xxgR7bS-w5JXuNPDAA5g/Canon-PIXMA-MX925-Bedienungsanleitung-2efef0.pdf">Benutzerhandbuch</a>
                 </CardBody>
-<<<<<<< HEAD
                 <div style={{paddingLeft: '20%', paddingRight: '20%', paddingBottom: '4%'}}>
                     <Button style={{width: '100%', height: '100%', backgroundColor: '#00BCD4'}} variant="contained" size="large" color="primary" className={classes.button}>
                         Bestellung
@@ -82,12 +69,6 @@ function Hardware(props) {
                         <AccessTime/>updated 4 minutes ago
                     </div>
 
-=======
-                <CardFooter chart>
-                    <div className={classes.stats}>
-                        <AccessTime /> updated 4 minutes ago
-                    </div>
->>>>>>> salagic
                 </CardFooter>
             </Card>
         </GridItem>
@@ -95,11 +76,7 @@ function Hardware(props) {
         <GridItem xs={12} sm={6} md={4}>
             <Card chart>
                 <CardHeader>
-<<<<<<< HEAD
                     <img src={ require('../../assets/img/druckerxy.jpg') } width={"100%"} height={"100%"} mode="fit" />
-=======
-                    <img src={ require('../../assets/img/PIXMA MX925.jpg') } width={"100%"} height={"100%"}  mode="fit" />
->>>>>>> salagic
                 </CardHeader>
                 <CardBody>
                     <h4 className={classes.cardTitle}>Produkt XY</h4>
@@ -109,14 +86,11 @@ function Hardware(props) {
                     <p>Kategorie:</p><p> Drucker</p>
                     <a href="https://cdn.billiger.com/dynimg/BeJSIoTIRwE6Ls_wbObPEXt4H9rycHjeBa9LTdXG2DAE0q0O1YuZqiNdvIY_Nfu9H6xxgR7bS-w5JXuNPDAA5g/Canon-PIXMA-MX925-Bedienungsanleitung-2efef0.pdf">Benutzerhandbuch</a>
                 </CardBody>
-<<<<<<< HEAD
                 <div style={{paddingLeft: '20%', paddingRight: '20%', paddingBottom: '4%'}}>
                     <Button style={{width: '100%', height: '100%', backgroundColor: '#00BCD4'}} variant="contained" size="large" color="primary" className={classes.button}>
                         Bestellung
                     </Button>
                 </div>
-=======
->>>>>>> salagic
                 <CardFooter chart>
                     <div className={classes.stats}>
                         <AccessTime /> updated 4 minutes ago
@@ -128,11 +102,7 @@ function Hardware(props) {
         <GridItem xs={12} sm={6} md={4}>
             <Card chart>
                 <CardHeader>
-<<<<<<< HEAD
                     <img src={ require('../../assets/img/druckerxy.jpg') } width={"100%"} height={"100%"} mode="fit" />
-=======
-                    <img src={ require('../../assets/img/PIXMA MX925.jpg') } width={"100%"} height={"100%"}  mode="fit" />
->>>>>>> salagic
                 </CardHeader>
                 <CardBody>
                     <h4 className={classes.cardTitle}>Produkt XY</h4>
@@ -142,14 +112,11 @@ function Hardware(props) {
                     <p>Kategorie:</p><p> Drucker</p>
                     <a href="https://cdn.billiger.com/dynimg/BeJSIoTIRwE6Ls_wbObPEXt4H9rycHjeBa9LTdXG2DAE0q0O1YuZqiNdvIY_Nfu9H6xxgR7bS-w5JXuNPDAA5g/Canon-PIXMA-MX925-Bedienungsanleitung-2efef0.pdf">Benutzerhandbuch</a>
                 </CardBody>
-<<<<<<< HEAD
                 <div style={{paddingLeft: '20%', paddingRight: '20%', paddingBottom: '4%'}}>
                     <Button style={{width: '100%', height: '100%', backgroundColor: '#00BCD4'}} variant="contained" size="large" color="primary" className={classes.button}>
                         Bestellung
                     </Button>
                 </div>
-=======
->>>>>>> salagic
                 <CardFooter chart>
                     <div className={classes.stats}>
                         <AccessTime /> updated 4 minutes ago
@@ -161,11 +128,7 @@ function Hardware(props) {
         <GridItem xs={12} sm={6} md={4}>
             <Card chart>
                 <CardHeader>
-<<<<<<< HEAD
                     <img src={ require('../../assets/img/druckerxy.jpg') } width={"100%"} height={"100%"} mode="fit" />
-=======
-                    <img src={ require('../../assets/img/PIXMA MX925.jpg') } width={"100%"} height={"100%"}  mode="fit" />
->>>>>>> salagic
                 </CardHeader>
                 <CardBody>
                     <h4 className={classes.cardTitle}>Produkt XY</h4>
@@ -175,14 +138,11 @@ function Hardware(props) {
                     <p>Kategorie:</p><p> Drucker</p>
                     <a href="https://cdn.billiger.com/dynimg/BeJSIoTIRwE6Ls_wbObPEXt4H9rycHjeBa9LTdXG2DAE0q0O1YuZqiNdvIY_Nfu9H6xxgR7bS-w5JXuNPDAA5g/Canon-PIXMA-MX925-Bedienungsanleitung-2efef0.pdf">Benutzerhandbuch</a>
                 </CardBody>
-<<<<<<< HEAD
                 <div style={{paddingLeft: '20%', paddingRight: '20%', paddingBottom: '4%'}}>
                     <Button style={{width: '100%', height: '100%', backgroundColor: '#00BCD4'}} variant="contained" size="large" color="primary" className={classes.button}>
                         Bestellung
                     </Button>
                 </div>
-=======
->>>>>>> salagic
                 <CardFooter chart>
                     <div className={classes.stats}>
                         <AccessTime /> updated 4 minutes ago
