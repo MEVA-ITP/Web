dde224ade13de921a9e1feeb4873e9bdab852b3d
ckreibich-tgm <ckreibich@student.tgm.ac.at>
2018-11-27 09:26:46 +0100

    Update Footer
    link eingebaut

--- MEVA/src/components/Footer/Footer.jsx
+++ MEVA/src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ function Footer({...props}) {
                 <p className={classes.right}>
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
-              <a href="#" className={classes.a}>
+              <a href="dashboard" className={classes.a}>
               O N Q U I P
             </a> from student for student and for the mark
           </span>
