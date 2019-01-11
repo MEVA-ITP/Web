d1476c5f9a7bcace74b66d54a9e5c60fe3f0502c
ckreibich-tgm <ckreibich@student.tgm.ac.at>
2019-01-11 13:07:56 +0100

    update
    bug fixed

--- MEVA/src/views/Login/login.jsx
+++ MEVA/src/views/Login/login.jsx
@@ -50,11 +50,7 @@ function SignIn(props) {
     const { classes } = props;
 
     return (
-<<<<<<< HEAD
-            <body>
-=======
         <div className={"body"}>
->>>>>>> 3cd3b3e4991f2b5e9d4bd42cb444395bf363b203
             <main className={classes.main}>
                 <CssBaseline />
                 <Paper className={classes.paper} >
@@ -89,11 +85,7 @@ function SignIn(props) {
                     </form>
                 </Paper>
             </main>
-<<<<<<< HEAD
-            </body>
-=======
         </div>
->>>>>>> 3cd3b3e4991f2b5e9d4bd42cb444395bf363b203
     );
 }
 
